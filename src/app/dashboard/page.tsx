65821e43e7fe01b900cc5746cc32b015b291423c
Joe Kody <samonguro.a@gmail.com>
2024-05-24 21:15:24 +0700

    ไข่บวม

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,23 +4,34 @@ import General from "@/components/AdminGeneralPanel"
 import User from "@/components/AdminUserPanel"
 
 export default function Admin() {
-    return (
-      <main className="flex min-h-screen flex-col gap-4 p-4 md:gap-8 md:p-10">
-        <h1 className="text-3xl text-center font-bold">DASHBOARD</h1>
-        <div className="flex grow justify-center">
-          <Tabs defaultValue="general" className="min-w-max w-[350px] sm:w-[450px] md:w-[600px] xl:w-[700px] flex flex-col justify-center gap-4">
-            <TabsList className=''>
-              <TabsTrigger className='w-full' value="general">General</TabsTrigger>
-              <TabsTrigger className='w-full' value="user">User</TabsTrigger>
-            </TabsList>
-            <TabsContent className='grow mt-0' value="general">
-                <General />
-            </TabsContent>
-            <TabsContent className='grow mt-0' value="user">
-                <User />
-            </TabsContent>
-          </Tabs>
-        </div>
-      </main>
-    )
+
+  return (
+    <main className="flex min-h-screen flex-col gap-4 p-4 md:gap-8 md:p-10">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">DASHBOARD</h1>
+        <h2 className='text-md text-muted-foreground'>General</h2>
+      </div>
+      <div className="flex grow justify-center">
+        <Tabs
+          defaultValue="general"
+          className="min-w-max w-[350px] sm:w-[450px] md:w-[600px] xl:w-[700px] flex flex-col justify-center gap-4"
+        >
+          <TabsList className="">
+            <TabsTrigger className="w-full" value="general">
+              General
+            </TabsTrigger>
+            <TabsTrigger className="w-full" value="user">
+              User
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent className="grow mt-0" value="general">
+            <General />
+          </TabsContent>
+          <TabsContent className="grow mt-0" value="user">
+            <User />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </main>
+  )
 }
--- src/components/AdminUserColumns.tsx
+++ src/components/AdminUserColumns.tsx
@@ -20,6 +20,10 @@ User schema, กุลืมไอน้อง
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Switch } from "@/components/ui/switch"
+
 
 export type UserData = {
     id: number
@@ -109,7 +113,7 @@ export const columns: ColumnDef<UserData>[] = [
       return (
         <div>
           <span className="flex items-center">
-            Role
+            Admin
             <Button
               variant="ghost"
               size="icon"
@@ -126,6 +130,51 @@ export const columns: ColumnDef<UserData>[] = [
         </div>
       )
     },
+    cell: ({ row }) => {
+      /*
+      return (
+        <>
+          <Badge variant={row.getValue("role") === "ADMIN" ? "default" : "outline"}>{row.getValue('role')}</Badge>
+        </>
+      )
+      */
+      return (
+        <div className="text-center">
+          <Switch checked={row.getValue('role') === 'ADMIN'} />
+        </div>
+      )
+    }
+  },
+  {
+    accessorKey: 'approved',
+    header: ({ column }) => {
+      return (
+        <div>
+          <span className="flex items-center">
+            Approved
+            <Button
+              variant="ghost"
+              size="icon"
+              className="border-0"
+              onClick={() =>
+                column.toggleSorting(
+                  column.getIsSorted() === 'asc' || !column.getIsSorted()
+                )
+              }
+            >
+              <ArrowUpDown className="w-4 h-4" />
+            </Button>
+          </span>
+        </div>
+      )
+    },
+    cell: ({ row }) => {
+      return (
+        <div className="text-center pr-4">
+          <Switch checked={row.getValue('approved')} />
+        </div>
+      )
+    }
   },
   {
     accessorKey: 'createdAt',
@@ -150,9 +199,19 @@ export const columns: ColumnDef<UserData>[] = [
         </div>
       )
     },
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('createdAt'))
+      console.log(date);
+
+      return (
+        <>
+          {date.toLocaleString()}
+        </>
+      )
+    },
   },
   {
-    accessorKey: 'createdAt',
+    accessorKey: 'updatedAt',
     header: ({ column }) => {
       return (
         <div>
@@ -174,28 +233,14 @@ export const columns: ColumnDef<UserData>[] = [
         </div>
       )
     },
-  },
-  {
-    accessorKey: 'approved',
-    header: ({ column }) => {
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('updatedAt'))
+      console.log(date);
+
       return (
-        <div>
-          <span className="flex items-center">
-            Approved
-            <Button
-              variant="ghost"
-              size="icon"
-              className="border-0"
-              onClick={() =>
-                column.toggleSorting(
-                  column.getIsSorted() === 'asc' || !column.getIsSorted()
-                )
-              }
-            >
-              <ArrowUpDown className="w-4 h-4" />
-            </Button>
-          </span>
-        </div>
+        <>
+          {date.toLocaleString()}
+        </>
       )
     },
   },
